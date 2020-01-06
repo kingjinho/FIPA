94c123eb4a4c74a2ea45fe9034e628f8b3fa07c9
brent <jinhoshin319@gmail.com>
2020-01-06 23:06:52 +0900

    Splash screen with colors..

--- app/src/main/java/com/example/fipa/MainActivity.kt
+++ app/src/main/java/com/example/fipa/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * todo
+         * 1. 기본정보
+         * 2. 회원가입되어있고 로그인 되어있다면
+         * 3. 안되어있을시 로그인 화면
+         */
     }
 }
---
+++ app/src/main/java/com/example/fipa/SplashFragment.kt
@@ -0,0 +1,66 @@
+package com.example.fipa
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by KING JINHO on 2020-01-06
+ */
+
+class SplashFragment: Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+
+
+}
