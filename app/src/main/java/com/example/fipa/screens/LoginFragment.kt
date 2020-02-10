5d60cf33bc9fec9bff42f9091369fa35d61379f0
brent <jinhoshin319@gmail.com>
2020-02-10 22:46:08 +0900

    added related observers

--- app/src/main/java/com/example/fipa/screens/LoginFragment.kt
+++ app/src/main/java/com/example/fipa/screens/LoginFragment.kt
@@ -7,15 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.fipa.R
 import com.example.fipa.databinding.FragmentLoginBinding
+import com.example.fipa.viewmodels.LoginViewModel
 
 /**
  * Created by KING JINHO on 2020-01-12
  */
 class LoginFragment : Fragment() {
 
-    private lateinit var mLoginFragmentBindingUtil : FragmentLoginBinding
+    private lateinit var mLoginFragmentBindingUtil: FragmentLoginBinding
+
+    private lateinit var mViewModel: LoginViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -30,7 +36,24 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mLoginFragmentBindingUtil = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        mLoginFragmentBindingUtil =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        mViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        mLoginFragmentBindingUtil.loginViewModel = mViewModel
+        mLoginFragmentBindingUtil.lifecycleOwner = this
+        mViewModel.email.observe(viewLifecycleOwner, Observer { text ->
+            mViewModel.setEmail(text)
+        })
+        mViewModel.businessLicense.observe(viewLifecycleOwner, Observer { text ->
+            mViewModel.setBusinessLicense(text)
+        })
+        mViewModel.password.observe(viewLifecycleOwner, Observer { text ->
+            mViewModel.setPassword(text)
+        })
+
+
+
+
         return mLoginFragmentBindingUtil.root
     }
 
