970db98e840fba26c9d1112dd2daa0ed5ee5ac19
brent <jinhoshin319@gmail.com>
2020-02-10 22:46:22 +0900

    added constructor and functions

--- app/src/main/java/com/example/fipa/models/Login.kt
+++ app/src/main/java/com/example/fipa/models/Login.kt
@@ -3,4 +3,37 @@ package com.example.fipa.models
 /**
  * Created by KING JINHO on 2020-01-08
  */
-data class Login (var nameCompany: String, var email: String, var password: String, var businessLicence: String)
+data class Login(
+    private var email: String,
+    private var password: String,
+    private var businessLicense: String
+) {
+
+    constructor() : this("", "", "")
+
+
+    public fun setEmail(email: String) {
+        this.email = email
+    }
+
+    public fun getEmail(): String {
+        return email
+    }
+
+    public fun setPassword(password: String) {
+        this.password = password
+    }
+
+    public fun getPassword(): String {
+        return password
+    }
+
+    public fun setBusinessLicense(businessLicense: String) {
+        this.businessLicense = businessLicense
+    }
+
+    public fun getBusinessLicense() : String {
+        return businessLicense
+    }
+
+}
