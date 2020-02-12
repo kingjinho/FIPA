08fb090329cc3d1ecb53901a1d100b2218d6898e
brent <jinhoshin319@gmail.com>
2020-02-12 22:50:40 +0900

    a package and class for extionsions

---
+++ app/src/main/java/com/example/fipa/extensions/ActivityExtentions.kt
@@ -0,0 +1,12 @@
+package com.example.fipa.extensions
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+/**
+ * Created by KING JINHO on 2020-02-12
+ */
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
