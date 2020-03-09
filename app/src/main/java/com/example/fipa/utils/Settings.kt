8fa494e3773d0b173a9a74d439c4e54bc9e16932
brent <jinhoshin319@gmail.com>
2020-03-09 23:10:11 +0900

    file for constant values

---
+++ app/src/main/java/com/example/fipa/utils/Settings.kt
@@ -0,0 +1,14 @@
+package com.example.fipa.utils
+
+/**
+ * Created by KING JINHO on 2020-03-09
+ */
+
+class Settings{
+    companion object{
+        var EMAIL : String = ""
+        var TOKEN : String = ""
+        const val AUTHENTICATION = "AUTHENTICATION"
+        const val ID = "ID"
+    }
+}
