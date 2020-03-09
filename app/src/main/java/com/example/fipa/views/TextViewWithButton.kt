eeacbff5f5add00b75917d9d8b98316803f42589
brent <jinhoshin319@gmail.com>
2020-03-09 23:11:33 +0900

    inflation issue WIP

--- app/src/main/java/com/example/fipa/views/TextViewWithButton.kt
+++ app/src/main/java/com/example/fipa/views/TextViewWithButton.kt
@@ -6,22 +6,27 @@ import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import com.example.fipa.R
 import kotlinx.android.synthetic.main.textview_with_button_all.view.*
 
 /**
  * Created by KING JINHO on 2020-01-29
  */
-class TextViewWithButton(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+class TextViewWithButton(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
     lateinit var buttonClickListener: OnButtonClickListener
     private var mTypedArray: TypedArray
+    private lateinit var mBindingUtil: DataBindingUtil
 
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.textview_with_button_all, this)
+        var inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+        mBindingUtil = DataBindingUtil.inflate(inflater as LayoutInflater, R.layout.textview_with_button_all, this, false)
+        //LayoutInflater.from(context).inflate(R.layout.textview_with_button_all, this)
         mTypedArray =
             context.theme.obtainStyledAttributes(attrs, R.styleable.TextViewWithButton, 0, 0)
         setSectionHeader()
@@ -61,10 +66,14 @@ class TextViewWithButton(context: Context, attrs: AttributeSet) : ConstraintLayo
 
 
     private fun setTextColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             tvSection.setTextColor(resources.getColor(R.color.primaryTextColor, null))
         else
             tvSection.setTextColor(resources.getColor(R.color.primaryTextColor))
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            tvSection.setTextColor(resources.getColor(R.color.primaryTextColor, null))
+        else
+            tvSection.setTextColor(resources.getColor(R.color.primaryTextColor))*/
     }
 
     interface OnButtonClickListener {
