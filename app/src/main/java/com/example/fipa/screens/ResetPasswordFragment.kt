a2f406d08975a68068f65dd9737dae4c59617619
brent <jinhoshin319@gmail.com>
2020-03-18 21:56:51 +0900

    creation of Fragments

---
+++ app/src/main/java/com/example/fipa/screens/ResetPasswordFragment.kt
@@ -0,0 +1,22 @@
+package com.example.fipa.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fipa.R
+
+/**
+ * Created by KING JINHO on 2020-03-11
+ */
+class ResetPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    }
+}
