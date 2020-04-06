9dbda779effb9836ded1e27b0726a95b9d2f74e7
brent <jinhoshin319@gmail.com>
2020-04-06 23:11:56 +0900

    rename of network service

--- app/src/main/java/com/example/fipa/network/FIPAAPI.kt
+++ app/src/main/java/com/example/fipa/network/FIPAAPI.kt
@@ -2,20 +2,17 @@ package com.example.fipa.network
 
 import com.example.fipa.BuildConfig
 import com.example.fipa.models.User
+import com.example.fipa.utils.ID
 import com.example.fipa.utils.Settings
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
-import java.io.BufferedInputStream
-import java.io.FileInputStream
-import java.io.InputStream
 import java.security.cert.CertificateFactory
 import java.util.concurrent.TimeUnit
 
@@ -56,13 +53,16 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 
-interface FIPAApiService {
+interface ApiService {
 
     @POST("user/{id}")
-    suspend fun signup(@Path("id") id: String, @Body user: User): Response<User>
+    suspend fun signUp(@Path("id") id: String, @Body user: User): Response<User>
 
-    @GET("user/authentication/{ID}")
-    suspend fun getAuthenticationStatus(@Path(Settings.ID) id: String): Response<Boolean>
+    @POST("user/auth")
+    suspend fun loginRequest(@Body user: User) : Response<User>
+
+    @GET("user/auth/{ID}")
+    suspend fun getAuthenticationStatus(@Path(ID) id: String): Response<Boolean>
 
     @PATCH("user/token")
     suspend fun reissueToken(@Body user: User) : Response<String>
@@ -70,16 +70,16 @@ interface FIPAApiService {
     @PATCH("user/password-reset")
     suspend fun reset()
 
-    @DELETE("user/{id}")
-    suspend fun unregister(@Path("id") id: String): Response<Boolean>
+    @DELETE("user/{ID}")
+    suspend fun unregister(@Path(ID) id: String): Response<Boolean>
 
     @GET("user/test/{ID}")
-    suspend fun test(@Path(Settings.ID) id: String): Response<Boolean>
+    suspend fun test(@Path(ID) id: String): Response<Boolean>
 
 }
 
-object FIPAApi {
-    val retrofitService: FIPAApiService by lazy {
-        retrofit.create(FIPAApiService::class.java)
+object FIPA_NETWORK {
+    val REMOTE_SERVICE: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
     }
 }
